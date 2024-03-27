f7c6d8510ba49211f6647db895e47f39776a4759
Frederik De Bleser <frederik@debleser.be>
2024-03-27 17:31:24 +0100

    Stuff

--- netlify/functions/generate-upload-url.js
+++ netlify/functions/generate-upload-url.js
@@ -4,8 +4,11 @@ const AWS = require("aws-sdk");
 const { v4: uuidv4 } = require("uuid");
 
 // Assuming AWS credentials are set up in environment variables or via AWS config
-const s3 = new AWS.S3();
-const bucketName = process.env.AWS_S3_BUCKET_NAME;
+const s3 = new AWS.S3({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+});
+const bucketName = process.env.S3_BUCKET_NAME;
 
 exports.handler = async (event, context) => {
   try {
